0d9b7e3aa8356a66a6b64fdd7abc48992f3a1070
upjd <qszhang19892008@163.com>
2015-12-19 21:02:40 +0800

    Initiation Test Create dd File

---
+++ GDWS/GDWS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GDWS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("General Electric")]
+[assembly: AssemblyProduct("GDWS")]
+[assembly: AssemblyCopyright("Copyright © General Electric 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02b8070c-fa5c-465c-ae75-e50e73107936")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GDWS/GDWS/Service1.asmx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace GDWS
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            TextWriter CsqWrite = new StreamWriter(@"D:\Temp\temp.dd");
+            CsqWrite.Write("%BTW% /F=");
+            CsqWrite.Write(" /D=%Trigger File Name% ");
+            CsqWrite.Write(" /prn=");
+            CsqWrite.WriteLine("/P /Close /FP /C=001 /DbTextHeader=3 /R=3");
+            CsqWrite.WriteLine("%END%");
+            CsqWrite.Write("DESCRIPTION,");
+            CsqWrite.Write("REVISION,");
+            CsqWrite.Write("COMCODE,");
+            CsqWrite.Write("QUANTITY,");
+            CsqWrite.Write("UNITSN,");
+            CsqWrite.Write("BOXNUMBER,");
+            CsqWrite.Write("SKIDNUMBER,");
+            CsqWrite.Write("CLEI,");
+            CsqWrite.Write("ECI,");
+            CsqWrite.Write("CUSTPARTNUMBER,");
+            CsqWrite.Write("CUSTREVISION,");
+            CsqWrite.Write("REPAIRSTATUS,");
+            CsqWrite.Write("REPAIRSTATUSBARCODE,");
+            CsqWrite.Write("ALUREF,");
+            CsqWrite.Write("LOCATION,");
+            CsqWrite.Write("ROHSLOGO,");
+            CsqWrite.Write("SFTYMARK1,");
+            CsqWrite.Write("SFTYMARK2,");
+            CsqWrite.Write("SFTYMARK3,");
+            CsqWrite.Write("SFTYMARK4,");
+            CsqWrite.Write("SFTYMARK5,");
+            CsqWrite.Write("SFTYMARK6,");
+            CsqWrite.Write("DATECODE,");
+            CsqWrite.Write("DATECODEBARCODE,");
+            CsqWrite.Write("RMA,");
+            CsqWrite.Write("ORDER,");
+            CsqWrite.Write("SN1,");
+            CsqWrite.Write("SN2,");
+            CsqWrite.Write("SN3,");
+            CsqWrite.Write("SN4,");
+            CsqWrite.Write("SN5,");
+            CsqWrite.Write("SN6,");
+            CsqWrite.Write("SN7,");
+            CsqWrite.Write("SN8,");
+            CsqWrite.Write("SN9,");
+            CsqWrite.WriteLine("SN10");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Write(",");
+            CsqWrite.Close();
+            return "Hello";
+        }
+    }
+}
